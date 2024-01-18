efc8ec03c03cadcc6cc357f0d4bba55b7c8b3c26
TomEssers <75224810+TomEssers@users.noreply.github.com>
2024-01-18 08:59:09 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -25,10 +25,17 @@ icao = df_icao.select('ICAO').rdd.flatMap(lambda x: x).collect()
 # Import all flight data of the period Jan 2020 till ... into a dataframe
 df_flights = spark.read.csv(flight_data_path, header=True, inferSchema=True)
 
-df1 = df_flights.filter(df_flights.origin.startswith("EH") & df_flights.destination.substr(1,2).isin(icao) & ~df_flights.origin.isin(["EHVK","EHDB","EHDP","EHGR","EHDS","EHND","EHMZ","EHDR","EHHV","EHLW","EHWO","EHOW"]) & (df_flights.origin != df_flights.destination))
+# Filter for data where the start is the Netherlands (EH) and the destination is europe
+# Additionally remove flights that have same origin as destination
+df1 = df_flights.filter(
+    ~df_flights.origin.isin(["EHVK", "EHDB", "EHDP", "EHGR", "EHDS", "EHND", "EHMZ", "EHDR", "EHHV", "EHLW", "EHWO", "EHOW"]) &
+    df_flights.destination.substr(1, 2).isin(icao) & (df_flights.origin != df_flights.destination))
+
+
 df1 = df1.withColumn("filename", regexp_extract(input_file_name(), r"flightlist_(\d{8}_\d{8})", 1))
 df1 = df1.withColumn("firstseen", to_timestamp(df1.firstseen))
 df1 = df1.withColumn("lastseen", to_timestamp(df1.lastseen))
-df1 = df1.withColumn("time_difference", col("lastseen").cast("long") - col("firstseen").cast("long"))  
+df1 = df1.withColumn("time_difference", col("lastseen").cast("long") - col("firstseen").cast("long"))
 
-df1.select(df1.origin, df1.destination, df1.time_difference, df1.firstseen, df1.filename).write.mode("append").csv("/user/s2484765/testing_project")
+df1.select(df1.origin, df1.destination, df1.time_difference, df1.firstseen, df1.filename).write.mode("append").csv(
+    "/user/s2484765/testing_project")
